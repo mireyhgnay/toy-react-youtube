47ed4ebaba78d7d50cf88ba79c5c4f6babad80ef
hyerim <hyerimiya1216@gmail.com>
2023-07-18 21:34:06 +0900

    Feat : 검색 기능 구현 진행 중

--- youtube/src/components/Header.jsx
+++ youtube/src/components/Header.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsYoutube } from "react-icons/bs";
+import { GoSearch } from "react-icons/go";
 
-export default function Header() {
-  return <header>HEADER FIXED📌</header>;
+export default function Header({}) {
+  const [text, setText] = useState("");
+
+  return (
+    <header className="header">
+      <h1 className="header_logo">
+        <BsYoutube />
+        <a href="/">YOUTUBE</a>
+      </h1>
+
+      <form name="search_form" action="">
+        <label htmlFor="header_search"></label>
+        <input
+          type="text"
+          id="header_search"
+          className="header_search_input"
+          placeholder="검색"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <button>
+          <GoSearch />
+        </button>
+      </form>
+    </header>
+  );
 }
